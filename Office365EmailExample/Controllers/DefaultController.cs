5f1fea6f489f4f02fee9d2743ab3de3dbd61c4c8
Auri A. Rahimzadeh <auri.rahimzadeh@stonegatemtg.com>
2015-08-04 10:32:43 -0400

    Added controller, view, and model that were missing from initial commit for some reason.

---
+++ Office365EmailExample/Controllers/DefaultController.cs
@@ -0,0 +1,71 @@
+﻿using Office365EmailExample.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Office365EmailExample.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        [ActionName("Index")]
+        [HttpGet]
+        public ActionResult IndexGet()
+        {
+            return View();
+        }
+
+        [ActionName("Index")]
+        [HttpPost]
+        public ActionResult IndexPost(EmailMessage model)
+        {
+            if (ModelState.IsValid)
+            {
+                ViewBag.Success = SendMail(model);
+                return View(model);
+            }
+            else return View(model);
+        }
+
+        private bool SendMail(EmailMessage model)
+        {
+            var result = false;
+
+            using (MailMessage msg = new MailMessage())
+            {
+                // Configure the message.
+                msg.To.Add(new MailAddress(model.RecipientAddress, "Recipient"));
+                msg.From = new MailAddress(model.CredentialsUsername, model.CredentialsFullName); // Must EXACTLY match the account you're sending from! Easy way to find this is send an email to an external address and see the displayed name.
+                msg.Subject = model.Subject;
+                msg.Body = model.Body;
+                msg.IsBodyHtml = false; // Modify as necessary, unimportant to getting email working.
+
+                // Attempt to send.
+                using (SmtpClient client = new SmtpClient())
+                {
+                    client.UseDefaultCredentials = false;
+                    client.Credentials = new System.Net.NetworkCredential(model.CredentialsUsername, model.CredentialsPassword);
+                    client.Port = 587; // You can use Port 25 if 587 is blocked, but SSL may not work, and that's baaaad, m'kay?
+                    client.Host = "smtp.office365.com";
+                    client.DeliveryMethod = SmtpDeliveryMethod.Network;
+                    client.EnableSsl = true;
+                    try
+                    {
+                        client.Send(msg);
+                        result = true;
+                    }
+                    catch (Exception ex)
+                    {
+                        ViewBag.ErrorMessage = ex.Message;
+                        result = false;
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Office365EmailExample/Models/EmailMessage.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Office365EmailExample.Models
+{
+    public class EmailMessage
+    {
+        [Required]
+        [StringLength(128)]
+        [MinLength(6)]
+        [DataType(DataType.EmailAddress)]
+        [Display(Name="Recipient Address")]
+        [RegularExpression(@"^[A-Za-z0-9](([_\.\-]?[a-zA-Z0-9]+)*)@([A-Za-z0-9]+)(([\.\-]?[a-zA-Z0-9]+)*)\.([A-Za-z]{2,})$")]
+        public string RecipientAddress { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        [MinLength(1)]
+        public string Subject { get; set; }
+
+        [Required]
+        [StringLength(1000)]
+        [MinLength(2)]
+        public string Body { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Office 365 Password")]
+        public string CredentialsPassword { get; set; }
+
+        [Required]
+        [DataType(DataType.Text)]
+        [Display(Name = "Office 365 Username")]
+        public string CredentialsUsername { get; set; }
+
+        [Required]
+        [DataType(DataType.Text)]
+        [Display(Name = "Office 365 User Email Display Name")]
+        public string CredentialsFullName { get; set; }
+
+
+    }
+}
